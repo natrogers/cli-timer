7395dc2822b253ec17f07a3bee78ac4c31d05dc5
Nat Rogers <npr3ha@virginia.edu>
2019-01-17 16:03:15 -0500

    Add cli-timer.py outline

---
+++ cli-timer.py
@@ -0,0 +1,35 @@
+#! usr/bin/env python3
+
+# @author: N Rogers
+
+import sys
+import time
+import subprocess
+import argparse
+
+
+def handle_args():
+    pass
+
+
+def convert_to_sec(arg_time):
+    pass
+
+
+def timer(sec):
+    # https://www.geeksforgeeks.org/timer-objects-python/
+    # https://stackoverflow.com/questions/18406165/creating-a-timer-in-python
+    pass
+
+
+def print_to_terminal():
+    # https://stackoverflow.com/questions/2122385/dynamic-terminal-printing-with-python
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
